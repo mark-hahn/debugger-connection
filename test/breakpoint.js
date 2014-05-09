cee569db514923af2f6642fa9e3bc025d934d660
kiddkai <kiddkai@gmail.com>
2014-05-09 15:20:16 +0800

    add breakpoint

---
+++ test/breakpoint.js
@@ -0,0 +1,22 @@
+var BreakPoint = require('../lib/breakpoint')
+var sinon = require('sinon')
+
+
+describe('Break Point', function() {
+
+  var breakpoint;
+
+  beforeEach(function() {
+
+  })
+
+  describe('list', function() {
+
+    it('should try to consume remote debugger of all the break points', function() {
+      var cb = 
+      BreakPoint.list()
+    })
+
+  })
+
+})
